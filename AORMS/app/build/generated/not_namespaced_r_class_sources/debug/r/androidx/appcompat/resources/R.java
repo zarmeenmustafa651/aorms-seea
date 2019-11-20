451eded547955ca0d12e1905a4659ed5d6cc0712
zarmeen <zarmeen651@gmail.com>
2019-11-20 05:21:45 +0500

    prototype 2

--- AORMS/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/appcompat/resources/R.java
+++ AORMS/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/appcompat/resources/R.java
@@ -23,15 +23,15 @@ public final class R {
         public static final int fontStyle = 0x7f0300ec;
         public static final int fontVariationSettings = 0x7f0300ed;
         public static final int fontWeight = 0x7f0300ee;
-        public static final int ttcIndex = 0x7f03021a;
+        public static final int ttcIndex = 0x7f03021e;
     }
     public static final class color {
         private color() {}
 
         public static final int notification_action_color_filter = 0x7f050078;
         public static final int notification_icon_bg_color = 0x7f050079;
-        public static final int ripple_material_light = 0x7f050085;
-        public static final int secondary_text_default_material_light = 0x7f050087;
+        public static final int ripple_material_light = 0x7f050086;
+        public static final int secondary_text_default_material_light = 0x7f050088;
     }
     public static final class dimen {
         private dimen() {}
@@ -112,40 +112,40 @@ public final class R {
         public static final int accessibility_custom_action_7 = 0x7f080036;
         public static final int accessibility_custom_action_8 = 0x7f080037;
         public static final int accessibility_custom_action_9 = 0x7f080038;
-        public static final int action_container = 0x7f080040;
-        public static final int action_divider = 0x7f080042;
-        public static final int action_image = 0x7f080043;
-        public static final int action_text = 0x7f080049;
-        public static final int actions = 0x7f08004a;
-        public static final int async = 0x7f080053;
-        public static final int blocking = 0x7f080058;
-        public static final int chronometer = 0x7f080076;
-        public static final int dialog_button = 0x7f080094;
-        public static final int forever = 0x7f0800b3;
-        public static final int icon = 0x7f0800c4;
-        public static final int icon_group = 0x7f0800c5;
-        public static final int info = 0x7f0800cd;
-        public static final int italic = 0x7f0800cf;
-        public static final int line1 = 0x7f0800e0;
-        public static final int line3 = 0x7f0800e1;
-        public static final int normal = 0x7f0800f1;
-        public static final int notification_background = 0x7f0800f2;
-        public static final int notification_main_column = 0x7f0800f3;
-        public static final int notification_main_column_container = 0x7f0800f4;
-        public static final int right_icon = 0x7f080110;
-        public static final int right_side = 0x7f080111;
-        public static final int tag_accessibility_actions = 0x7f080153;
-        public static final int tag_accessibility_clickable_spans = 0x7f080154;
-        public static final int tag_accessibility_heading = 0x7f080155;
-        public static final int tag_accessibility_pane_title = 0x7f080156;
-        public static final int tag_screen_reader_focusable = 0x7f080157;
-        public static final int tag_transition_group = 0x7f080158;
-        public static final int tag_unhandled_key_event_manager = 0x7f080159;
-        public static final int tag_unhandled_key_listeners = 0x7f08015a;
-        public static final int text = 0x7f08015e;
-        public static final int text2 = 0x7f08015f;
-        public static final int time = 0x7f080190;
-        public static final int title = 0x7f080192;
+        public static final int action_container = 0x7f080041;
+        public static final int action_divider = 0x7f080043;
+        public static final int action_image = 0x7f080044;
+        public static final int action_text = 0x7f08004a;
+        public static final int actions = 0x7f08004b;
+        public static final int async = 0x7f080054;
+        public static final int blocking = 0x7f080059;
+        public static final int chronometer = 0x7f080078;
+        public static final int dialog_button = 0x7f080096;
+        public static final int forever = 0x7f0800b6;
+        public static final int icon = 0x7f0800c8;
+        public static final int icon_group = 0x7f0800c9;
+        public static final int info = 0x7f0800d1;
+        public static final int italic = 0x7f0800d3;
+        public static final int line1 = 0x7f0800e4;
+        public static final int line3 = 0x7f0800e5;
+        public static final int normal = 0x7f0800f6;
+        public static final int notification_background = 0x7f0800f7;
+        public static final int notification_main_column = 0x7f0800f8;
+        public static final int notification_main_column_container = 0x7f0800f9;
+        public static final int right_icon = 0x7f080115;
+        public static final int right_side = 0x7f080116;
+        public static final int tag_accessibility_actions = 0x7f080159;
+        public static final int tag_accessibility_clickable_spans = 0x7f08015a;
+        public static final int tag_accessibility_heading = 0x7f08015b;
+        public static final int tag_accessibility_pane_title = 0x7f08015c;
+        public static final int tag_screen_reader_focusable = 0x7f08015d;
+        public static final int tag_transition_group = 0x7f08015e;
+        public static final int tag_unhandled_key_event_manager = 0x7f08015f;
+        public static final int tag_unhandled_key_listeners = 0x7f080160;
+        public static final int text = 0x7f080164;
+        public static final int text2 = 0x7f080165;
+        public static final int time = 0x7f080196;
+        public static final int title = 0x7f080198;
     }
     public static final class integer {
         private integer() {}
@@ -158,10 +158,10 @@ public final class R {
         public static final int custom_dialog = 0x7f0b0038;
         public static final int notification_action = 0x7f0b0050;
         public static final int notification_action_tombstone = 0x7f0b0051;
-        public static final int notification_template_custom_big = 0x7f0b0052;
-        public static final int notification_template_icon_group = 0x7f0b0053;
-        public static final int notification_template_part_chronometer = 0x7f0b0054;
-        public static final int notification_template_part_time = 0x7f0b0055;
+        public static final int notification_template_custom_big = 0x7f0b0058;
+        public static final int notification_template_icon_group = 0x7f0b0059;
+        public static final int notification_template_part_chronometer = 0x7f0b005d;
+        public static final int notification_template_part_time = 0x7f0b005e;
     }
     public static final class string {
         private string() {}
@@ -173,11 +173,11 @@ public final class R {
 
         public static final int TextAppearance_Compat_Notification = 0x7f0f0119;
         public static final int TextAppearance_Compat_Notification_Info = 0x7f0f011a;
-        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f0f011b;
-        public static final int TextAppearance_Compat_Notification_Time = 0x7f0f011c;
-        public static final int TextAppearance_Compat_Notification_Title = 0x7f0f011d;
-        public static final int Widget_Compat_NotificationActionContainer = 0x7f0f01c6;
-        public static final int Widget_Compat_NotificationActionText = 0x7f0f01c7;
+        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f0f011c;
+        public static final int TextAppearance_Compat_Notification_Time = 0x7f0f011f;
+        public static final int TextAppearance_Compat_Notification_Title = 0x7f0f0121;
+        public static final int Widget_Compat_NotificationActionContainer = 0x7f0f01cb;
+        public static final int Widget_Compat_NotificationActionText = 0x7f0f01cc;
     }
     public static final class styleable {
         private styleable() {}
@@ -208,7 +208,7 @@ public final class R {
         public static final int FontFamily_fontProviderFetchTimeout = 3;
         public static final int FontFamily_fontProviderPackage = 4;
         public static final int FontFamily_fontProviderQuery = 5;
-        public static final int[] FontFamilyFont = { 0x1010532, 0x1010533, 0x101053f, 0x101056f, 0x1010570, 0x7f0300e4, 0x7f0300ec, 0x7f0300ed, 0x7f0300ee, 0x7f03021a };
+        public static final int[] FontFamilyFont = { 0x1010532, 0x1010533, 0x101053f, 0x101056f, 0x1010570, 0x7f0300e4, 0x7f0300ec, 0x7f0300ed, 0x7f0300ee, 0x7f03021e };
         public static final int FontFamilyFont_android_font = 0;
         public static final int FontFamilyFont_android_fontWeight = 1;
         public static final int FontFamilyFont_android_fontStyle = 2;
--- AORMS/app/src/main/java/com/example/aorms/Report.java
+++ AORMS/app/src/main/java/com/example/aorms/Report.java
@@ -3,9 +3,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Report extends AppCompatActivity {
 
@@ -16,6 +23,23 @@ public class Report extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.tToolbar);
         setSupportActionBar(myToolbar);
         myToolbar.setTitle("                REPORTS");
+        final GraphView graph = (GraphView) findViewById(R.id.graph);
+        //Button button = findViewById(R.id.addButton);
+        graph.setVisibility(View.VISIBLE);
+        Integer firstInput_1=1, secondInput_1=2,firstInput_2=3, secondInput_2=4,firstInput_3=5, secondInput_3=6,firstInput_4=7, secondInput_4=8;
+
+        try {
+            LineGraphSeries <DataPoint> series = new LineGraphSeries< >(new DataPoint[] {
+                    new DataPoint(0, 1),
+                    new DataPoint(firstInput_1,secondInput_1),
+                    new DataPoint(firstInput_2,secondInput_2),
+                    new DataPoint(firstInput_3,secondInput_3),
+                    new DataPoint(firstInput_4,secondInput_4)
+            });
+            graph.addSeries(series);
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(Report.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
